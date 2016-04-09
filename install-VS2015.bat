92ccf2ee2beb12ac6594b824863ae6a2cef6bcff
Nithin S Warrier <nithin486@hotmail.com>
2016-04-09 18:25:57 +0530

    Update Install-VS2015.bat
    Deleted del command to stop error file not found and set %ProgramFiles% if Windows is 32bit

--- install-VS2015.bat
+++ install-VS2015.bat
@@ -1,8 +1,10 @@
 @REM This file is used to build the Cosmos sources.
 
 cls
-del source\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.*
-
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+if %OS%==64BIT
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" "source\Builder.sln" /nologo /maxcpucount /p:Configuration=Debug /p:Platform=x86
-
+start source\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.exe -bat -VS2015 %1 %2 %3 %4 %5 %6 %7 %8 %9
+if %OS%==32BIT
+"%ProgramFiles%\MSBuild\14.0\Bin\msbuild.exe" "source\Builder.sln" /nologo /maxcpucount /p:Configuration=Debug /p:Platform=x86
 start source\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.exe -bat -VS2015 %1 %2 %3 %4 %5 %6 %7 %8 %9
